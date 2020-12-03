8e3ea5ad3758bd1df44c4de58e7f1ce38f751420
markdrehmann <markdrehmann@gmail.com>
2020-12-02 20:42:30 -0800

    created tables in migrations

--- db/migrate/20201203042908_create_makers.rb
+++ db/migrate/20201203042908_create_makers.rb
@@ -1,4 +1,12 @@
 class CreateMakers < ActiveRecord::Migration
   def change
+    create_table :makers do |t|
+      t.string :name
+      t.string :nationality
+      t.integer :year_born
+      t.integer :year_died
+      t.string :city
+      t.string :bio
+    end
   end
 end
