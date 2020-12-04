e4180ed314d04c462fd0aebe30a1b4754d965e94
markdrehmann <markdrehmann@gmail.com>
2020-12-03 20:49:24 -0800

    added migration to change 'user' to 'username'

---
+++ db/migrate/20201204043614_change_user_to_username_in_users.rb
@@ -0,0 +1,5 @@
+class ChangeUserToUsernameInUsers < ActiveRecord::Migration
+  def change
+    rename_column :users, :user, :username
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201203042908) do
+ActiveRecord::Schema.define(version: 20201204043614) do
 
   create_table "bows", force: :cascade do |t|
     t.integer "user_id"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20201203042908) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
   end
