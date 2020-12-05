75af2c486ad67bfa5754676d29f3c41ea67213ac
markdrehmann <markdrehmann@gmail.com>
2020-12-04 21:40:57 -0800

    added redirect_if_logged_in to a bunch of routes

--- app/controllers/bows_controller.rb
+++ app/controllers/bows_controller.rb
@@ -1,16 +1,19 @@
 class BowsController < ApplicationController
 
     get '/bows' do
+        redirect_if_not_logged_in
         @bows = Bow.all
         erb :'/bows/index'
     end
 
     get '/bows/new' do
+        redirect_if_not_logged_in
         erb :'/bows/new'
     end
 
     get '/bows/:id' do
-
+        redirect_if_not_logged_in
+        @bow = Bow.find(params[:id])
         erb :'/bows/show'
     end
 end
