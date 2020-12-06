54de43d04363ef000cde8594bb831abc3e028ec4
markdrehmann <markdrehmann@gmail.com>
2020-12-06 12:31:33 -0600

    added get /makers and makers/:id, started views for both

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,4 +1,15 @@
 class MakersController < ApplicationController
 
+    get '/makers' do
+        redirect_if_not_logged_in
+        @makers = Maker.all
+        erb :'/makers/index'
+    end
+
+    get '/makers/:id' do
+        redirect_if_not_logged_in
+        @maker = Maker.find(params[:id]) # NOT SURE IF THIS IS CORRECT PARAMS
+        erb :'/makers/show'
+    end
 
 end
